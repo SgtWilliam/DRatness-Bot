2ac3e8010a0447d863f186e555425185601c2c28
Sgt_William <williampaes097@gmail.com>
2022-03-25 18:04:10 -0300

    Marcando @everyone no twitter

--- src/BotModules/TwitterNotification.js
+++ src/BotModules/TwitterNotification.js
@@ -12,7 +12,7 @@ const TwitterNotification = {
             consumer_secret:      process.env.TWITTER_CONSUMER_KEY_SECRET,
             access_token:         process.env.TWITTER_ACCESS_TOKEN,
             access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
-            timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+            timeout_ms:           60*100,  // optional HTTP request timeout to apply to all requests.
             strictSSL:            true,     // optional - requires SSL certificates to be valid.
         })
 
@@ -25,9 +25,10 @@ const TwitterNotification = {
             if(tweet.user.id == TWITTER_USER_WATCH_ID) {
                 let url = "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
                 try {
+                    let sendMessage = "@everyone "+ url;
                     let channel = discordClient.channels.fetch(TWITTER_DiscordChannelID).then(channel => {
-                        channel.send(url)
-                        console.log(url)
+                        channel.send(sendMessage)
+                        console.log(sendMessage)
                     }).catch(err => {
                         console.log(err)
                     })
